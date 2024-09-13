d3c51d95e7e00a3856ff4d023affd90e5606b8a5
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-09-13 23:28:11 +0600

    Build the connection to the mysql in the backend

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "Project-370",
+  password: "Rafi123@",
+});
+
+app.listen(8081, () => {
+  console.log("Server is running on port 8081");
+});
